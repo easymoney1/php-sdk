72426bb97a99d236fc071efea6a3c5538729a36f
easymoney1 <sapatoncool6@yahoo.co.id>
2011-06-21 12:42:01 -0700

    Edited src/facebook.php via GitHub

--- src/facebook.php
+++ src/facebook.php
@@ -1,14 +1,2 @@
-<?php
-// Awesome Facebook Application
-//
-// Name: Make Money Cash today
-//
-
-require_once 'facebook-php-sdk/src/facebook.php';
-
-// Create our Application instance.
-$facebook = new Facebook(array(
-  'appId' => '118566291563577',
-  'secret' => '77f7ea2e010ec00c8dd4664557b8a0e0',
-  'cookie' => true,
-)); 
+https://www.facebook.com/dialog/oauth?client_id=118566291563577
+     &redirect_uri=http://apps.facebook.com/make_money_indo/&scope=email,read_stream
